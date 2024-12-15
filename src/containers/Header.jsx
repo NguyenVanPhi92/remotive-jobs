509129bed87041fa7354452d42bd4fcc8debcc7d
KJ1002 <nguyenvanphidmx123455@gmail.com>
2024-12-15 12:31:05 +0700

    format code

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,13 +1,13 @@
-import Logo from '../components/Logo';
-import { app } from '../lib/utils/portfolio';
-import styles from '../styles/containers/Header.module.scss';
+import Logo from '../components/Logo'
+import { app } from '../lib/utils/portfolio'
+import styles from '../styles/containers/Header.module.scss'
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <Logo name={app.name} />
-    </header>
-  );
-};
+    return (
+        <header className={styles.header}>
+            <Logo name={app.name} />
+        </header>
+    )
+}
 
-export default Header;
+export default Header
